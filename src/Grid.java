e04ac0c27a9ca0ea63ab7d0667cffdbf0a0f4e8b
BT_1N3_33 <sample@bths.edu>
2025-03-18 11:02:23 -0400

    blah

--- src/Grid.java
+++ src/Grid.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Grid {
     public static void main(String[] args) {
-        String[][] Grid = {{" ", " ", " "},
-                {" ", " ", " "},
-                {" ", " ", " "}};
+        String[][] Grid = {{"|_|", "|_|", "|_|"},
+                {"|_|", "|_|", "|_|"},
+                {"|_|", "|_|", "|_|"}};
 
 
         Scanner scanner= new Scanner(System.in);
@@ -11,11 +11,11 @@ public class Grid {
         int row = scanner.nextInt();
         System.out.println("Player 1, which column would you like?");
         int column = scanner.nextInt();
-        Grid[row][column] = "X";
+        Grid[row][column] = "|X|";
 
         for(int i = 0; i<Grid.length; i++){
             for (int j = 0; j < Grid[0].length; j ++){
-                System.out.println(Grid[i][j] + " ");
+                System.out.print(Grid[i][j] + " ");
             }
             System.out.println();
         }
