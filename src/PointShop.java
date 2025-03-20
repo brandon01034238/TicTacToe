c67318b54295db6df3e14ad3e2c509cb06950c46
BT_1N3_34 <sample@bths.edu>
2025-03-20 11:03:04 -0400

    blah

--- src/PointShop.java
+++ src/PointShop.java
@@ -2,6 +2,11 @@ public class PointShop extends DiffGameMode {
     private int currentPoints;
 
     public PointShop(int currentPoints) {
+        this.currentPoints = currentPoints;
+        super()
+    }
 
+    public int getCurrentPoints() {
+        return currentPoints;
     }
 }
